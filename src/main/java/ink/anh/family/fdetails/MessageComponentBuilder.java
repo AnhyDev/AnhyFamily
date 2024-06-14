f069d3618fc758154d530c26b3f218200ad88272
AnhyDev <support@anh.ink>
2024-06-14 13:42:53 +0300

    2024-06-14-13-42

--- src/main/java/ink/anh/family/fdetails/MessageComponentBuilder.java
+++ src/main/java/ink/anh/family/fdetails/MessageComponentBuilder.java
@@ -12,57 +12,64 @@ import ink.anh.family.GlobalManager;
 public class MessageComponentBuilder {
 
     private static GlobalManager libraryManager = GlobalManager.getInstance();
-    
+    private static final String MESSAGE_COLOR = "#0bdebb";
+    private static final String GROUP_COLOR = "#0bdebb";
+    private static final String ACCESS_COLOR_TRUE = "#00FF00";
+    private static final String ACCESS_COLOR_FALSE = "#FF0000";
+    private static final String ACCESS_COLOR_DEFAULT = "#FFFF00";
+    private static final String SEPARATOR_COLOR = "#cedcf2";
+
     private static String getAccessColor(Access access) {
-        return access == Access.TRUE ? "#00FF00" : access == Access.FALSE ? "#FF0000" : "#FFFF00";
+        return access == Access.TRUE ? ACCESS_COLOR_TRUE : access == Access.FALSE ? ACCESS_COLOR_FALSE : ACCESS_COLOR_DEFAULT;
     }
-    
+
     private static String stringSetAccess(Access access, String[] langs) {
         return StringUtils.colorize(StringUtils.formatString(Translator.translateKyeWorld(libraryManager, "family_hover_set_default", langs),
-                new String[] {access.name()}));
+                new String[]{access.name()}));
         // family_hover_set_default: "Встановити доступ %s"
     }
-    
+
+    private static String colorize(String startColor, String element) {
+        return startColor + element + MESSAGE_COLOR;
+    }
+
     public static MessageComponents buildDefaultAccessMessage(Player player, String group, Access access, String baseCommand) {
         String command = "/" + baseCommand + " default ";
         String accessColor = getAccessColor(access);
-        String groupColor = "#0bdebb";
-        String messageColor = "#0bdebb";
         
         String[] langs = player != null ? LangUtils.getPlayerLanguage(player) : new String[]{libraryManager.getDefaultLang()};
-        
+
         String groupAccess = StringUtils.colorize(StringUtils.formatString(Translator.translateKyeWorld(libraryManager, baseCommand.toUpperCase() + " family_message_group_access", langs),
-                new String[] {groupColor + group + messageColor, accessColor + access.name() + messageColor}));
+                new String[]{colorize(GROUP_COLOR, group), colorize(accessColor, access.name())}));
         // family_message_group_access: "У групи %s доступ %s, щоб змінити доступ оберіть варіант: "
-        
+
         return MessageComponents.builder()
             .content(groupAccess)
-            .hexColor(messageColor)
+            .hexColor(MESSAGE_COLOR)
             .append(MessageComponents.builder()
-                .content("allow")
-                .hexColor("#00FF00")
+                .content("ALLOW")
+                .hexColor(ACCESS_COLOR_TRUE)
                 .hoverMessage(stringSetAccess(Access.TRUE, langs))
                 .clickActionRunCommand(command + group + " allow")
                 .build())
             .append(MessageComponents.builder()
                 .content(" | ")
-                .hexColor("#cedcf2")
+                .hexColor(SEPARATOR_COLOR)
                 .build())
             .append(MessageComponents.builder()
-                .content("deny")
-                .hexColor("#FF0000")
+                .content("DENY")
+                .hexColor(ACCESS_COLOR_FALSE)
                 .hoverMessage(stringSetAccess(Access.FALSE, langs))
                 .clickActionRunCommand(command + group + " deny")
                 .build())
             .build();
     }
-    
+
     public static MessageComponents buildCheckAccessMessage(Player player, String nickname, Access access, String baseCommand) {
         String command = "/" + baseCommand + " access ";
-        String messageColor = "#0bdebb";
-        
+
         String[] langs = player != null ? LangUtils.getPlayerLanguage(player) : new String[]{libraryManager.getDefaultLang()};
-        
+
         String accessStatus;
         switch (access) {
             case TRUE:
@@ -79,35 +86,42 @@ public class MessageComponentBuilder {
         }
 
         String checkAccessMessage = StringUtils.colorize(StringUtils.formatString(Translator.translateKyeWorld(libraryManager, "family_access_get", langs),
-                new String[] {nickname, accessStatus}));
+                new String[]{nickname, colorize(getAccessColor(access), accessStatus.toUpperCase())}));
         // family_access_get: "Гравець %s має доступ: %s"
-        
+
+        String changeAccessMessage = StringUtils.colorize(StringUtils.formatString(Translator.translateKyeWorld(libraryManager, "family_access_change", langs), new String[]{}));
+        // family_access_change: "Змінити доступ: "
+
         return MessageComponents.builder()
             .content(checkAccessMessage)
-            .hexColor(messageColor)
+            .hexColor(MESSAGE_COLOR)
             .append(MessageComponents.builder()
-                .content("allow")
-                .hexColor("#00FF00")
+                .content(changeAccessMessage)
+                .hexColor(MESSAGE_COLOR)
+                .build())
+            .append(MessageComponents.builder()
+                .content("ALLOW")
+                .hexColor(ACCESS_COLOR_TRUE)
                 .hoverMessage(stringSetAccess(Access.TRUE, langs))
                 .clickActionRunCommand(command + nickname + " allow")
                 .build())
             .append(MessageComponents.builder()
                 .content(" | ")
-                .hexColor("#cedcf2")
+                .hexColor(SEPARATOR_COLOR)
                 .build())
             .append(MessageComponents.builder()
-                .content("deny")
-                .hexColor("#FF0000")
+                .content("DENY")
+                .hexColor(ACCESS_COLOR_FALSE)
                 .hoverMessage(stringSetAccess(Access.FALSE, langs))
                 .clickActionRunCommand(command + nickname + " deny")
                 .build())
             .append(MessageComponents.builder()
                 .content(" | ")
-                .hexColor("#cedcf2")
+                .hexColor(SEPARATOR_COLOR)
                 .build())
             .append(MessageComponents.builder()
-                .content("default")
-                .hexColor("#FFFF00")
+                .content("DEFAULT")
+                .hexColor(ACCESS_COLOR_DEFAULT)
                 .hoverMessage(stringSetAccess(Access.DEFAULT, langs))
                 .clickActionRunCommand(command + nickname + " default")
                 .build())
