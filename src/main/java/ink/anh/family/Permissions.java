41853ac218eb9dd92d103136c7cdf03b465d1e6a
AnhyDev <support@anh.ink>
2024-07-22 23:10:57 +0300

    2024-07-22-23-10

--- src/main/java/ink/anh/family/Permissions.java
+++ src/main/java/ink/anh/family/Permissions.java
@@ -1,10 +1,9 @@
 package ink.anh.family;
 
 public class Permissions {
-    public static final String RELOAD = "anhyfamily.reload";
+    public static final String FAMILY_ADMIN = "family.admin";
     public static final String FAMILY_USER = "family.user";
     public static final String FAMILY_PASTOR = "family.pastor";
-    public static final String FAMILY_ADMIN = "family.admin";
     public static final String FAMILY_TPHOME = "family.tphome";
     public static final String FAMILY_CHEST_IGNORE_DISTANCE = "family.chest.ignoreDistance";
     public static final String FAMILY_CHEST_IGNORE_WORLD = "family.chest.ignoreWorld";
