87c594eab36b27e4456117fbfda60fdafc90ddd7
AnhyDev <support@anh.ink>
2024-07-23 23:00:07 +0300

    2024-07-23-23-00

--- src/main/java/ink/anh/family/Permissions.java
+++ src/main/java/ink/anh/family/Permissions.java
@@ -5,8 +5,8 @@ public class Permissions {
     public static final String FAMILY_USER = "family.user";
     public static final String FAMILY_PASTOR = "family.pastor";
     public static final String FAMILY_TPHOME = "family.tphome";
-    public static final String FAMILY_CHEST_IGNORE_DISTANCE = "family.chest.ignoreDistance";
-    public static final String FAMILY_CHEST_IGNORE_WORLD = "family.chest.ignoreWorld";
+    public static final String FAMILY_CHEST_IGNORE_DISTANCE = "family.chest.ignore.distance";
+    public static final String FAMILY_CHEST_IGNORE_WORLD = "family.chest.ignore.world";
     public static final String FAMILY_CHEST_CLICK = "family.chest.click";
     public static final String FAMILY_CHEST_BREAK = "family.chest.break";
 }
